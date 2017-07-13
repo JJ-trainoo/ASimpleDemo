bfdc027fc53bb80e4c4f2fe1deb861cdb6918bf5
zhoutao <trainoo@163.com>
2017-07-13 20:23:20 +0800

    统计出现次数最多的单词

---
+++ src/main/java/com/trainoo/demo/Demo_01.java
@@ -0,0 +1,49 @@
+package com.trainoo.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 统计出现次数最多的单词
+ * 需求：
+ * 	给定一个文本，统计里面出现次数最多的单词
+ * 	1.忽略大小写
+ * 	2.假定文中只有，。？ 以及空格 四种字符。
+ * 
+ * @author zhoutao
+ * @date 2017年7月13日
+ */
+public class Demo_01 {
+
+	public static String maxCountWord(String text) {
+		// 分割字符串
+		String[] wordList = text.replace(",", " ").replace(".", " ").replace("?", " ").split(" ");
+		// 单词做key 次数作为 value 存入map
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < wordList.length; i++) {
+			String word = wordList[i].toLowerCase();
+			if(null == map.get(word)){
+				map.put(word, 1);
+			}else{
+				map.put(word, map.get(word) + 1);
+			}
+		}
+		System.out.println(map);
+		// 遍历map,取出出现最多次的单词
+		int maxCount = 0;
+		String result = "";
+		for (String key : map.keySet()){
+			if(maxCount < map.get(key) && !"".equals(key)){
+				maxCount = map.get(key);
+				result = key;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		String text = "How are you, i am fin thank you, and you?";
+		System.out.println(maxCountWord(text));
+	}
+
+}
