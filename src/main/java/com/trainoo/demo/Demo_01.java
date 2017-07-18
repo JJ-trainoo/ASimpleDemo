76c579f647fbbec3fb7994458098f0ed920052a3
zhoutao <trainoo@163.com>
2017-07-18 22:17:15 +0800

    修改demo01

--- src/main/java/com/trainoo/demo/Demo_01.java
+++ src/main/java/com/trainoo/demo/Demo_01.java
@@ -8,15 +8,16 @@ import java.util.Map;
  * 需求：
  * 	给定一个文本，统计里面出现次数最多的单词
  * 	1.忽略大小写
- * 	2.假定文中只有，。？ 以及空格 四种字符。
- * 
+ * 	2.假定文中只有，。？ 以及空格 四种字符需要处理。
+ *  eg:  如：“How are you, how is everything going?”
+ *		 return： how
  * @author zhoutao
  * @date 2017年7月13日
  */
 public class Demo_01 {
 
 	public static String maxCountWord(String text) {
-		// 分割字符串
+		// 分割字符串,此处为了方便， 使用多次replace简单处理
 		String[] wordList = text.replace(",", " ").replace(".", " ").replace("?", " ").split(" ");
 		// 单词做key 次数作为 value 存入map
 		Map<String, Integer> map = new HashMap<String, Integer>();
@@ -28,7 +29,7 @@ public class Demo_01 {
 				map.put(word, map.get(word) + 1);
 			}
 		}
-		System.out.println(map);
+		//System.out.println(map);
 		// 遍历map,取出出现最多次的单词
 		int maxCount = 0;
 		String result = "";
